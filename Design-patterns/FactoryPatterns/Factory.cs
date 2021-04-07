c90ba1ee017c09047a2bf198d2f170cf51d829c5
FiletDrengen <sami.amir.assad@gmail.com>
2021-04-07 11:05:35 +0200

    Factory patterns mappe

---
+++ Design-patterns/FactoryPatterns/Factory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design_patterns
+{
+    public abstract class Factory
+    {
+        public abstract GameObject Create(string type);
+
+    }
+
+}
+
